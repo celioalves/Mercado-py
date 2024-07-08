bf46eaf2a6d9b6c7e46deab4d9b2b6e65c4ef01f
celioalves <craftsman7@gmail.com>
2024-07-08 19:06:16 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,13 @@
+from models.produto import Produto
+
+
+ps5 = Produto('Playstation 5', 3841.32)
+xbox = Produto('Xbox Series X', 4321.48)
+
+print(ps5)
+print(xbox)
+
+
+print('=' * 40)
+print('=' * 13 + ' Bem-Vindo(a) ' + '=' * 13)
+print('=' * 14 + ' Mercado ' + '=' * 17)
